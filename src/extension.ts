c8871c2cc6fc3046d7fb99ef7e6bf384e3b5ac33
7TIN <prasadgujarathi619@gmail.com>
2025-07-16 02:37:44 +0530

    feat: add extension show path functionility

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This is the main function that runs when your extension is activated.
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nami" is now active!');
+    console.log('Your "File Path Hint" extension is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('nami.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Nami!');
-	});
+    // Create a provider for our inlay hints.
+    const provider: vscode.InlayHintsProvider = {
+        
+        // This is the core function that provides the hints.
+        provideInlayHints(document, range) {
 
-	context.subscriptions.push(disposable);
+            // Get the relative path of the current file.
+            // This makes the path shorter and more readable (e.g., src/app.ts instead of C:/Users/...)
+            const relativePath = vscode.workspace.asRelativePath(document.uri);
+            
+            // Define the position for the hint: line 0, character 0 (the very top).
+            const position = new vscode.Position(0, 0);
+
+            // Create the inlay hint object.
+            const hint = new vscode.InlayHint(
+                position, // The position where the hint will be shown
+                `// ${relativePath}`, // The text that will be displayed
+                vscode.InlayHintKind.Parameter // The style of the hint
+            );
+            
+            // Add a helpful tooltip when the user hovers over the hint.
+            const markdownTooltip = new vscode.MarkdownString();
+            markdownTooltip.appendMarkdown(`**File Path:** \`${relativePath}\``);
+            markdownTooltip.appendMarkdown(`\n\nThis is a virtual hint provided by the extension and is not part of the actual file content.`);
+            hint.tooltip = markdownTooltip;
+            
+            // Add some padding to the left to make it look nice and align with code.
+            hint.paddingLeft = true;
+
+            // Return an array containing our single hint.
+            // VS Code will then render it in the editor.
+            return [hint];
+        }
+    };
+
+    // Now, we register our provider.
+    // The selector { pattern: '**/*' } tells VS Code to run this for all files.
+    const disposable = vscode.languages.registerInlayHintsProvider({ pattern: '**/*' }, provider);
+
+    // Add the provider to the context's subscriptions.
+    // This ensures it's cleaned up properly when the extension is deactivated.
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// This function is called when your extension is deactivated.
 export function deactivate() {}
